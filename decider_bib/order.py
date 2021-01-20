8eda9b30893373d210feb91e1d5cfefbe7019075
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-01-20 19:45:08 +0100

    fix(order): fix theissen sort order

--- decider_bib/order.py
+++ decider_bib/order.py
@@ -12,13 +12,20 @@
 Sorting
 -------
 
-We sort by family name and as tybreaker by year. If no name is given, we
-use `o. V. = ohne Verfasser` instead. If no year is given, we sort it
-after the items with year.
+Theissen
+~~~~~~~~
+
+We sort by family name and as tiebreaker by year.
+If no name is given, we use `o. V. = ohne Verfasser` instead.
+If no year is given, we sort it after the items with year.
+
+Publication without a person as authors, Siemens for example, arg
+treated as normal author but there must (Hrsg.) added.
 
 # TODO: ADD OTHER SORTING AS THEISSEN recommends
 """
 
+import iamraw
 import texmex.alpha
 import utila
 
@@ -33,9 +40,15 @@ def theissen_sort(items):
         key=lambda x: utila.INF if x.year in ('no year', None) else x.year,
     )
     # sort by author name
-    items = sorted(
-        items,
-        key=lambda x: texmex.alpha.replace(x.author).lower()
-        if x.author else 'o. V.',
-    )
+    items = sorted(items, key=author)
     return items
+
+
+def author(item: iamraw.BibliographyReference) -> str:
+    if not item.authors:
+        return 'o. V.'
+    if item.author:
+        # Person
+        return texmex.alpha.replace(item.author).lower()
+    # NoPerson
+    return item.authors[0].raw.lower()

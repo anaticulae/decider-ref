48760e2a4863fede86ae15bedd5161d2136b5db7
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-02-19 09:28:07 +0100

    feat(bib): disable plot on to few bib items

--- decider_bib/features/plot.py
+++ decider_bib/features/plot.py
@@ -9,17 +9,24 @@
 
 import collections
 
+import configo
 import painter
 import utila
 
 import decider_bib.order
 import decider_bib.serialize
 
+BILIOGRAPHY_MIN_COUNT = configo.HV_INT_PLUS(default=10)
+
 
 def work(table: str) -> bytes:
     bibliography = decider_bib.serialize.load_bibliography_reference(table)
 
-    rendered = render_year_overview(bibliography)
+    rendered = None
+    if len(bibliography) >= BILIOGRAPHY_MIN_COUNT:
+        rendered = render_year_overview(bibliography)
+    else:
+        utila.debug(f'too few bib items: {len(bibliography)}')
     if not rendered:
         # no bibs available
         return utila.NO_RESULT

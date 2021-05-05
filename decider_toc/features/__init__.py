50cb2b1f743ab770a12b6a34818f578cdb309dd4
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-05-05 10:27:02 +0200

    refactor(remove): remove outdated code

--- decider_toc/features/__init__.py
+++ decider_toc/features/__init__.py
@@ -6,11 +6,3 @@
 # use or distribution is an offensive act against international law and may
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
-
-import iamraw
-
-# TODO: MOVE TO PROTOCOL
-# ERRORS WHICH ARE NOT BOUND ON A SPECIAL PAGE AND ARE SHOW ON A SUMMARY
-# PAGE FOR EXAMPLE THE LAST OR FIRST PAGE.
-# TODO: REPLACE WITH -1 ONE AFTER FIXING PROTOCOL
-OVERVIEW = iamraw.Location.from_page(-1)

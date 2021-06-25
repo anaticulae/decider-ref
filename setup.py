805c8c0b10b5300d1e13d7054531308e843f830c
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-06-25 13:18:00 +0200

    style(refactor): cleanup setup

--- setup.py
+++ setup.py
@@ -14,21 +14,15 @@ import re
 import setuptools
 
 ROOT = os.path.abspath(os.path.dirname(__file__))
-UTF8 = 'utf8'
 
-with open(os.path.join(ROOT, 'README.md'), mode='rt', encoding=UTF8) as fp:
+with open(os.path.join(ROOT, 'README.md'), encoding='utf8') as fp:
     README = fp.read()
 
-with open(os.path.join(ROOT, 'decider_ref/__init__.py'),
-          mode='rt',
-          encoding=UTF8) as fp:
+with open(os.path.join(ROOT, 'decider_ref/__init__.py'), encoding='utf8') as fp:
     VERSION = re.search(r'__version__ = \'(.*?)\'', fp.read()).group(1)
 
-with open(os.path.join(ROOT, "requirements.txt"), mode='rt',
-          encoding=UTF8) as fp:
-    INSTALL_REQUIRES = [
-        line for line in fp.readlines() if line and '#' not in line
-    ]
+with open(os.path.join(ROOT, "requirements.txt"), encoding='utf8') as fp:
+    REQUIRES = [line for line in fp.readlines() if line and '#' not in line]
 
 if __name__ == "__main__":
     # allow ssetup.py to run from another directory
@@ -37,7 +31,7 @@ if __name__ == "__main__":
         author='Helmut Konrad Fahrendholz',
         author_email='info@checkitweg.de',
         description='checker',
-        install_requires=INSTALL_REQUIRES,
+        install_requires=REQUIRES,
         long_description=README,
         name='decider_ref',
         platforms='any',
@@ -47,6 +41,7 @@ if __name__ == "__main__":
         classifiers=[
             'Programming Language :: Python :: 3.7',
             'Programming Language :: Python :: 3.8',
+            'Programming Language :: Python :: 3.9',
         ],
         packages=[
             'decider_abb',

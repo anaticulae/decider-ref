01bf59f817b35875c3380351542ce826510d3283
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2021-08-01 18:08:36 +0200

    test(refactor): clarify code

--- tests/conftest.py
+++ tests/conftest.py
@@ -24,17 +24,17 @@ WORKER = 6
 RESOURCES = [
     (power.MASTER116_PDF, '0:50,75:115'),
     (power.MASTER098_PDF, '0:10,43:65,88:97'),
-    (power.BACHELOR076_PDF, ':'),
-    (power.MASTER072_PDF, ':'),
-    (power.BACHELOR090_PDF, ':'),
+    power.BACHELOR076_PDF,
+    power.MASTER072_PDF,
+    power.BACHELOR090_PDF,
     (power.BACHELOR128_PDF, '0:70'),
-    (power.BACHELOR037_PDF, ':'),
-    (power.MASTER099_PDF, '0:10'),
-    (power.TECH024_PDF, '0:15'),
+    power.BACHELOR037_PDF,
     (power.BACHELOR056_PDF, '0:15,49:55'),
     (power.BACHELOR063_PDF, '0:20,59'),
     (power.BACHELOR051_PDF, '0:10,40:52'),
     (power.MASTER091B_PDF, '0:20,81:91'),
+    (power.TECH024_PDF, '0:15'),
+    (power.MASTER099_PDF, '0:10'),
     (power.MASTER127_PDF, '121:126'),
     (power.MASTER083_PDF, '75:82'),
     (power.ORDER107_PDF, '104:110'),
@@ -61,6 +61,7 @@ def extract(resources):
         full=True,
         base=power.REPOSITORY,
         worker=WORKER,
+        pages=':',
     )
 
 

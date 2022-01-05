673e0ac76f7c87faef8d17970ff8bb2335955015
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-01-05 12:56:57 +0100

    chore(resources): extend test data generator

--- tests/conftest.py
+++ tests/conftest.py
@@ -44,7 +44,7 @@ RESOURCES = [
     (power.BACHELOR075_PDF, '15:25,70:74'),
     (power.BACHELOR241_PDF, '0:10,235:242'),
     (power.DISS266_PDF, '0:10,214:246'),
-    (power.DISS172_PDF, '152:172'),
+    (power.DISS172_PDF, '100:172'),
     (power.DISS406_PDF, '3:12'),
 ]
 

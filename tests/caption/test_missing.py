b6a6044dd51c8d3b02bf825aa618ab3f801d68cc
Helmut Konrad Fahrendholz <helmi3000@outlook.com>
2022-04-28 12:39:57 +0200

    test(caption): add missing figure caption test

---
+++ tests/caption/test_missing.py
@@ -0,0 +1,23 @@
+# =============================================================================
+# C O P Y R I G H T
+# -----------------------------------------------------------------------------
+# Copyright (c) 2022 by Helmut Konrad Fahrendholz. All rights reserved.
+# This file is property of Helmut Konrad Fahrendholz. Any unauthorized copy,
+# use or distribution is an offensive act against international law and may
+# be prosecuted under federal law. Its content is company confidential.
+# =============================================================================
+
+import power
+import protocol
+
+import tests.caption
+
+
+def test_caption_missing_tech019(testdir, monkeypatch):
+    source = power.link(power.TECH019_PDF)
+    tests.caption.run(
+        f'-i {source} -o {testdir.tmpdir} --missing',
+        monkeypatch=monkeypatch,
+    )
+    findings = protocol.findings_from_path(testdir.tmpdir)
+    assert findings

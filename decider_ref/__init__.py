814fb8410019aa2de90203a6a75900989ddb9359
Helmut Konrad Schewe <helmutus@outlook.com>
2026-08-05 01:03:31 +0200

    chore(pyproject): add modern layout

--- decider_ref/__init__.py
+++ decider_ref/__init__.py
@@ -7,9 +7,10 @@
 # be prosecuted under federal law. Its content is company confidential.
 # =============================================================================
 
+import importlib.metadata
 import os
 
-__version__ = '1.22.1'
+__version__ = importlib.metadata.version('decider_ref')
 
 ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 PACKAGE = 'decider_ref'
